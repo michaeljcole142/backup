818b5280c5dd93dc0857ceea4f103573adab0b58
Michael Cole <michaeljcole142@gmail.com>
2020-03-18 19:08:11 -0400

    initial create

---
+++ custom_components/switch/switch_example.py
@@ -0,0 +1,38 @@
+"""
+Copy this file to <config_dir>/switch/example.py
+
+Add to your configuration.yaml:
+
+switch:
+  platform: example
+  file_path: /tmp/HELLO_WORLD
+"""
+import os
+from homeassistant.helpers.entity import ToggleEntity
+
+
+def setup_platform(hass, config, add_devices, discovery_info=None):
+    add_devices([FileSwitch(config['file_path'])])
+
+
+class FileSwitch(ToggleEntity):
+    def __init__(self, path):
+        self.path = path
+        self.update()
+
+    @property
+    def name(self):
+        return os.path.basename(self.path)
+
+    @property
+    def is_on(self):
+        return self._state
+
+    def turn_on(self, **kwargs):
+        open(self.path, 'a').close()
+
+    def turn_off(self, **kwargs):
+        os.remove(self.path)
+
+    def update(self):
+        self._state = os.path.isfile(self.path)
